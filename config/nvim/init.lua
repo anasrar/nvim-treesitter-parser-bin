7743c16ef51d5c93e9293ef17b6d5dbe68c7d92c
anasrar <mynameanasrar@gmail.com>
2022-12-25 16:23:43 +1300

    feat: change compiler `windows` to `mingw` and `schedule` every 2 days
    - delete workflow run

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,7 +6,7 @@ return require('packer').startup(function()
   use {
     'nvim-treesitter/nvim-treesitter',
     config = function()
-      require('nvim-treesitter.install').compilers = {'cl', 'gcc' }
+      require('nvim-treesitter.install').compilers = {'x86_64-w64-mingw32-gcc-win32', 'gcc' }
       require("nvim-treesitter.install").prefer_git = true
       require('nvim-treesitter.configs').setup({
         ignore_install = { 'phpdoc' }, -- https://github.com/nvim-treesitter/nvim-treesitter/issues/2837
